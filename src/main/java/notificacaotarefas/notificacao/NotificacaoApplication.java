5cc511ddae5038865a5b17038c227c9ef02a3eb2
Arthur <arthurdev.contatoo@gmail.com>
2026-07-29 10:48:48 -0300

    iniciando projeto

---
+++ src/main/java/notificacaotarefas/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package notificacaotarefas.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}
